723175729c9709f5635e65fe8d4ca14d163333de
108sci <108.scientific@gmail.com>
2025-04-02 04:43:51 +0000

    Added Art Class to activities list

--- src/app.py
+++ src/app.py
@@ -38,6 +38,12 @@ activities = {
         "schedule": "Mondays, Wednesdays, Fridays, 2:00 PM - 3:00 PM",
         "max_participants": 30,
         "participants": ["john@mergington.edu", "olivia@mergington.edu"]
+    },
+    "Art Class": {
+        "description": "Explore creativity through painting, drawing, and sculpture",
+        "schedule": "Wednesdays, 4:00 PM - 5:30 PM",
+        "max_participants": 15,
+        "participants": ["john@mergington.edu","stella@mergington.edu"]
     }
 }
 
